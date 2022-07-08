6944ff30d28ba6d5e434811e69b8e31aa76c0741
Ali Efe <65660565+A713F3@users.noreply.github.com>
2022-07-09 00:10:28 +0300

    added random colors

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 #include <fstream>
+#include <map>
+#include <random>
 using namespace std;
 
+typedef struct color{
+    int r, g, b;
+} color;
+
 int main(int argc, char** argv){
+    // Random Color Generator
+    default_random_engine generator;
+    uniform_int_distribution<int> distribution(0, 255);
+
     if (argc == 1){
         cerr << "Enter file name!" << endl;
         return 0;
@@ -19,12 +29,34 @@ int main(int argc, char** argv){
     }
     cerr << "File succesfully loaded" << endl;
 
+    map<char, color> colors;
+
+    color blank_color = {255, 255, 255};
+    colors[' '] = blank_color;
+
     int width = 0, height = -1;
     string line;
 
     // Finding width and height of the output image
+    // And creating random colors for each character
+    cerr << "\n-Colors:" << endl;
     while (file){
         getline(file, line);
+
+        for (int i = 0; i < line.length(); i++){
+            char c = line[i];
+            if (colors.find(c) == colors.end()){
+                int r = distribution(generator);
+                int g = distribution(generator);
+                int b = distribution(generator);
+
+                cerr << c << ": " << r << ',' << g << ',' << b << endl;
+
+                color new_color = {r, g, b};
+
+                colors[c] = new_color;
+            }
+        }
         
         if (line.length() > width) width = line.length();
         height++;
@@ -35,7 +67,7 @@ int main(int argc, char** argv){
         return 0;
     }
 
-    cerr << "Output Image Dimensions:" << endl;
+    cerr << "\n-Output Image Dimensions:" << endl;
     cerr << "Width:  " << width << endl;
     cerr << "Height: " << height << endl; 
 
@@ -47,14 +79,24 @@ int main(int argc, char** argv){
 
     for (int i = 0; i < height; i++){
         getline(file, line);
+        int len = line.length();
 
-        for (int j = 0; j < width; j++){
-            char ch = line[j];
-            int c = (int)ch;
+        for (int l = 0; l < len; l++){
+            color char_color = colors[line[l]]; 
 
-            cout << c << ' ' << c << ' ' << c << '\n';
+            int r = char_color.r;
+            int g = char_color.g;
+            int b = char_color.b;
+
+            cout << r << ' ' << g << ' ' << b << '\n';
+        }
+
+        for (int j = 0; j < width - len; j++){
+            cout << 255 << ' ' << 255 << ' ' << 255 << '\n';
         }
     }
+
+    cerr << "Image successfully rendered!" << endl;
 
     return 0;
 }
